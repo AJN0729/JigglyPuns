3fea7f0d1ce643cde13f4f5f4a5272eb01681919
AJN0729 <andrewn0729@student.k12.tulsaschools.org>
2022-04-19 10:12:19 -0500

    added constants class.

---
+++ app/src/main/java/com/example/jigglypuns/Constants.kt
@@ -0,0 +1,141 @@
+package com.example.jigglypuns
+
+object Constants {
+
+    fun getQuestions(): ArrayList<Question> {
+
+        val questionsList = ArrayList<Question>()
+
+        val que1 = Question(
+            1,
+            "Question 1",
+            R.drawable.jigglypuff,
+            "Option 1",
+            "Option 2",
+            "Option 3",
+            "Option 4",
+            1
+        )
+
+        questionsList.add(que1)
+
+        val que2 = Question(
+            2,
+            "Question 1",
+            R.drawable.jigglypuff,
+            "Option 1",
+            "Option 2",
+            "Option 3",
+            "Option 4",
+            1
+        )
+
+        questionsList.add(que2)
+
+        val que3 = Question(
+            3,
+            "Question 1",
+            R.drawable.jigglypuff,
+            "Option 1",
+            "Option 2",
+            "Option 3",
+            "Option 4",
+            1
+        )
+
+        questionsList.add(que3)
+
+        val que4 = Question(
+            4,
+            "Question 1",
+            R.drawable.jigglypuff,
+            "Option 1",
+            "Option 2",
+            "Option 3",
+            "Option 4",
+            1
+        )
+
+        questionsList.add(que4)
+
+        val que5 = Question(
+            5,
+            "Question 1",
+            R.drawable.jigglypuff,
+            "Option 1",
+            "Option 2",
+            "Option 3",
+            "Option 4",
+            1
+        )
+
+        questionsList.add(que5)
+
+        val que6 = Question(
+            6,
+            "Question 1",
+            R.drawable.jigglypuff,
+            "Option 1",
+            "Option 2",
+            "Option 3",
+            "Option 4",
+            1
+        )
+
+        questionsList.add(que6)
+
+        val que7 = Question(
+            7,
+            "Question 1",
+            R.drawable.jigglypuff,
+            "Option 1",
+            "Option 2",
+            "Option 3",
+            "Option 4",
+            1
+        )
+
+        questionsList.add(que7)
+
+        val que8 = Question(
+            8,
+            "Question 1",
+            R.drawable.jigglypuff,
+            "Option 1",
+            "Option 2",
+            "Option 3",
+            "Option 4",
+            1
+        )
+
+        questionsList.add(que8)
+
+        val que9 = Question(
+            9,
+            "Question 1",
+            R.drawable.jigglypuff,
+            "Option 1",
+            "Option 2",
+            "Option 3",
+            "Option 4",
+            1
+        )
+
+        questionsList.add(que9)
+
+        val que10 = Question(
+            10,
+            "Question 1",
+            R.drawable.jigglypuff,
+            "Option 1",
+            "Option 2",
+            "Option 3",
+            "Option 4",
+            1
+        )
+
+        questionsList.add(que10)
+
+        return questionsList
+    }
+}
--- app/src/main/java/com/example/jigglypuns/Question.kt
+++ app/src/main/java/com/example/jigglypuns/Question.kt
@@ -1,11 +1,9 @@
 package com.example.jigglypuns
 
-import android.media.Image
-
 data class Question(
     val id: Int,
     val question: String,
-    val image: Image,
+    val image: Int,
     val optionOne: String,
     val optionTwo: String,
     val optionThree: String,
